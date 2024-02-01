49345b28a3da6e2320f478f65d18acb7186f0a2c
Dilshad <hmydilshadkt@gmail.com>
2024-02-01 13:07:10 +0530

    fix:contact section

--- app/(root)/contact/ContactBox.tsx
+++ app/(root)/contact/ContactBox.tsx
@@ -6,17 +6,17 @@ import WifiCalling3Icon from "@mui/icons-material/WifiCalling3";
 const ContactBox = () => {
   return (
     <div className="flex sm:flex-col my-[5%] md:pb-[15%] ">
-      <div className="p-[4%] md:p-3 border flex-1 flex flex-col items-center justify-center m-3 rounded-xl">
+      <div className="p-[4%] md:p-3 border-[1px] border-red-200 sm:border-red-300 flex-1 flex flex-col items-center justify-center m-3 rounded-xl">
         <LocationOnIcon className="text-red-500 text-[50px] my-3" />
         <h4 className="text-center md:text-sm">
           THAR PLAZA, KACHERIPADI - PANDIKKAD RD BYPASS, MANJERI, KERALA 676121
         </h4>
       </div>
-      <div className="p-[4%] border flex-1 flex flex-col items-center justify-center m-3 rounded-xl">
+      <div className="p-[4%] border-[1px] border-red-200 sm:border-red-300 flex-1 flex flex-col items-center justify-center m-3 rounded-xl">
         <MailOutlineIcon className="text-red-500  text-[50px] my-3" />
         <h4 className="text-center">zigzagmpm@gmail.com</h4>
       </div>
-      <div className="p-[4%] border flex-1 flex flex-col items-center justify-center m-3 rounded-xl">
+      <div className="p-[4%] border-[1px] border-red-200 sm:border-red-300 flex-1 flex flex-col items-center justify-center m-3 rounded-xl">
         <WifiCalling3Icon className="text-red-500  text-[50px] my-3" />
         <h4 className="text-center">
           +91 994-644-3551 <br />
